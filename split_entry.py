d7660d06d6080f3ab0f725accb11f66cf8b1c8b1
TungKhanhTran <tung.tran.1805@hcmut.edu.vn>
2021-01-26 14:25:24 +0700

    Code tách mục từ cho từ điển Ban tu thư, vẫn còn nhiều chỗ sai do chất lượng từ điển thấp

--- split_entry.py
+++ split_entry.py
@@ -16,12 +16,35 @@ def splitImageToEntries(imageNames, inputsDir, columnsDir):
         
         print('---------------------',image)
         img = cv2.imread(inputsDir + '/' + image) # Read in the image and convert to grayscale
-        h, w = img.shape[:2]
-        # cv2.imshow("cropped", cv2.resize(gray, (int(img.shape[1]/5), int(img.shape[0]/5))))
+        H, W = img.shape[:2]
+        # cv2.imshow("cropped", cv2.resize(img, (int(img.shape[1]/5), int(img.shape[0]/5))))
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if isRightColumn:
+            gray = cv2.cvtColor(img[:-100, :-200], cv2.COLOR_BGR2GRAY)
+        else:
+            gray = cv2.cvtColor(img[:-100, :], cv2.COLOR_BGR2GRAY)
+        gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, np.ones((15, 15), dtype=np.uint8)) # Perform noise filtering
+        # cv2.imshow("filtered", cv2.resize(gray, (int(gray.shape[1]/7), int(gray.shape[0]/7))))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        gray = cv2.bitwise_not(gray)
+        coords = cv2.findNonZero(gray) # Find all non-zero points (text)
+        x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
+        if W > 2000 and isRightColumn:
+            img = img[:, :-200]
+            rect = img[:, x - 20 if x - 20 > 0 else x:x+w+50] # Crop the image - note we do this on the original image
+        else:
+            rect = img[:, x - 20 if x - 20 > 0 else x:]
+        img = rect
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = img
+        # cv2.imshow("gray", cv2.resize(img, (int(gray.shape[1]/7), int(gray.shape[0]/7))))z
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
         # threshold:
         th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
         
@@ -34,18 +57,29 @@ def splitImageToEntries(imageNames, inputsDir, columnsDir):
             w,h = h,w
             ang += 90
 
+
         # Find rotated matrix, do rotation:
-        M = cv2.getRotationMatrix2D((cx,cy), ang, flip)
+        
+        if isRightColumn and -90 < ang < 90:
+            M = cv2.getRotationMatrix2D((cx,cy), 180 - ang, 1)
+        elif not isRightColumn and (ang > 90 or ang < -90):
+            M = cv2.getRotationMatrix2D((cx,cy), 180 - ang, 1)
+        else:
+            M = cv2.getRotationMatrix2D((cx,cy), ang, 1)
         rotated = cv2.warpAffine(threshed, M, (img.shape[1], img.shape[0]))
-
-
         # Find and draw the upper and lower boundary of each lines
-        hist = cv2.reduce(rotated,1, cv2.REDUCE_AVG).reshape(-1)
+        hist = cv2.reduce(rotated,1,cv2.REDUCE_AVG).reshape(-1)
 
+        # cv2.imshow("rotated", cv2.resize(rotated, (int(rotated.shape[1]/5), int(rotated.shape[0]/5))))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # print(flip, ang)
         th = 3 # 10: TD001, 5: TD002, 3: TD003
-        H,W = img.shape[:2]
+        H,W = rotated.shape[:2]
         uppers = [y for y in range(H-1) if hist[y]<=th and hist[y+1]>th]
         lowers = [y for y in range(H-1) if hist[y]>th and hist[y+1]<=th]
+        if lowers[0] < uppers[0]:
+            lowers = lowers[1:]
 
         rotated = cv2.bitwise_not(rotated)
         # rotated = cv2.cvtColor(rotated, cv2.COLOR_GRAY2BGR)
@@ -62,18 +96,23 @@ def splitImageToEntries(imageNames, inputsDir, columnsDir):
         # cv2.imwrite(columnsDir + '/' + image[0:-4] + '-' + 'test' + '.jpg', rotated)
 
         lineMinHeight = 50
-        lineMinLength = 1600 # 1350: TD001, 1800: TD002
-        newLineMaxStart = 50
+        # lineMinLength = 1600 # 1350: TD001, 1800: TD002
+        newLineMaxStart = 30
+        lastLineMaxLength = 50
         startEntry = 0
         endEntry = 0
         newEntry = False
         # print(len(uppers), len(lowers))
+        # cv2.imshow("drawed", cv2.resize(rotated, (int(rotated.shape[1]/7), int(rotated.shape[0]/7))))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
         for i in range(len(uppers)):
-            print(uppers[i] - lowers[i])
+            # print(uppers[i] - lowers[i])
             if (i == len(uppers) - 1): 
                 newEntry = True
                 if len(uppers) > len(lowers):
-                    endEntry = -1
+                    endEntry = rotated.shape[0] - 1
                 else:
                     endEntry = lowers[i]
             else:
@@ -88,20 +127,22 @@ def splitImageToEntries(imageNames, inputsDir, columnsDir):
                 coords = cv2.findNonZero(line) # Find all non-zero points (text)
                 x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
                 # Kết thúc một mục từ:
-                print(x, w)
+                # print(x, w)
                 # if w <= lineMinLength: # cho từ điển 001 và 002
                     # newEntry = True
                     # endEntry = lowers[i]
-                if (x < newLineMaxStart and not isRightColumn) or (x > newLineMaxStart and isRightColumn):
+                if (x < newLineMaxStart and not isRightColumn) or (x > lastLineMaxLength and isRightColumn):
                     newEntry = True
                     endEntry = uppers[i]
             if newEntry:
-                if isRightColumn:
-                    entry = cv2.rotate(rotated[startEntry:endEntry+5, :], cv2.ROTATE_180)
+                if endEntry == 0:
+                    pass
+                elif isRightColumn:
+                    entry = cv2.rotate(rotated[startEntry-30 if startEntry-30 > 0 else startEntry:endEntry, :], cv2.ROTATE_180)
                     cv2.imwrite(columnsDir + '/' + image[0:-4] + '-' + str(entryCount) + '.jpg', entry)
                     entryCount -= 1
                 else:
-                    entry = rotated[startEntry:endEntry+5, :]
+                    entry = rotated[startEntry-30 if startEntry-30 > 0 else startEntry:endEntry, :]
                     cv2.imwrite(columnsDir + '/' + image[0:-4] + '-' + str(entryCount) + '.jpg', entry)
                     entryCount += 1
                 # if not i + 1 == len(uppers):
@@ -116,5 +157,5 @@ if __name__ == "__main__":
     imageName = list(filter(lambda file: file[-3:] == 'jpg', os.listdir(inputDir)))
     columnDir = 'splitEntry/003'
 
-    splitImageToEntries(imageName, inputDir, columnDir)
-    # splitImageToEntries(['0030007-1.jpg'], inputDir, columnDir)
+    splitImageToEntries(imageName[1729:], inputDir, columnDir)
+    # splitImageToEntries(['0030867-1.jpg'], inputDir, columnDir)
